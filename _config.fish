cd1766a80a424d629479da8382fcc876605f4906
keita_takao <keita.takao.1112@gmail.com>
2022-02-06 02:39:23 +0900

    config.fishno

--- _config.fish
+++ _config.fish
@@ -1,24 +1,43 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-fish_add_path /usr/local/opt/mysql/bi :n
 
-fish_add_path $HOME/.composer/vendor/bin
-# fish_add_path /usr/local/opt/php@7.4/bin
-# fish_add_path /usr/local/opt/php@7.4/sbin
-fish_add_path $HOME/.nodebrew/current/bin
-status --is-interactive; and source (rbenv init -|psub)
- 
-fish_add_path /usr/local/opt/php@8.0/bin
-fish_add_path /usr/local/opt/php@8.0/sbin
+""""""""""""""""""""""""""""""
+" DB用
+""""""""""""""""""""""""""""""
+# Mysqlへのパス
+set PATH /usr/local/opt/mysql/bin $PATH
+
+""""""""""""""""""""""""""""""
+" Laravel 開発用
+""""""""""""""""""""""""""""""
+# composerへのパス 
+set PATH $HOME/.composer/vendor/bin $PATH
+
+set PATH  $HOME/.nodebrew/current/bin $PATH
+set PATH  /usr/local/opt/php@8.0/bin $PATH
+set PATH  /usr/local/opt/php@8.0/sbin $PATH
 
 
-# rmコマンドのalias登録
+
+
+
+"""""""""""""""""""""""""""""
+" alias 
+"""""""""""""""""""""""""""""
+# rmコマンドはゴミ箱に入る用に変更
 alias rm='trash'
 
+# git 関係
+alias gs='git status'
+alias gc='git commit -m'
+alias ga='git add'
+alias gp='git push'
+alisa gp='git pull'
+
 # nvimのエイリアス
-alias vi='nvim'
-alias vim='nvim'
+alias vi='vim'
+alias vim='vim'
 
 # python3をデフォルトにする
 alias python='python3'
