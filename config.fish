0f21ab2460fe39e1f2b3c9ea4d19a24e8ef00568
Takao <k.takao@neogenia.co.jp>
2022-04-23 09:41:38 +0900

    feat: set pyenv phpenv

--- config.fish
+++ config.fish
@@ -9,6 +9,14 @@ set -x PATH  /usr/local/opt/mysql/bin $PATH
 set -x  PATH $HOME/.rbenb/bin $PATH
 status --is-interactive; and source (rbenv init -|psub)
 
+# pyenv path
+set -x PATH $HOME/.pyenv/shims $PATH
+
+set -x PAHT /usr/local/opt/openssl@3/bin $PATH
+
+# phpbrew
+source ~/.phpbrew/phpbrew.fish
+
 # Laravel compos
 set -x PATH  $HOME/.composer/vendor/bin $PATH
 
@@ -16,8 +24,8 @@ set -x PATH  $HOME/.composer/vendor/bin $PATH
 set -x PATH  $HOME/.nodebrew/current/bin $PATH
 
 # PHP パス
-set -x PATH  /usr/local/opt/php@8.0/bin $PATH
-set -x  PATH /usr/local/opt/php@8.0/sbin $PATH
+# set -x PATH  /usr/local/opt/php@8.0/bin $PATH
+# set -x  PATH /usr/local/opt/php@8.0/sbin $PATH
 
 
 # rmコマンドのalias登録
@@ -39,3 +47,6 @@ alias pathfish='echo $fish_user_paths | tr " " "\n" | nl'
 
 # よく使用するディレクトリ
 alias paiza='cd /Users/keita_takao/learning/paiza'
+
+
+
