26c832b8ce978d49fbce7873bb7833897127be1a
Takao <k.takao@neogenia.co.jp>
2022-10-18 15:00:09 +0900

    fix: wezterm key binding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,18 @@
 local wezterm = require 'wezterm';
 
 return {
-  font = wezterm.font("HackGen35Nerd Console"),
+  font = wezterm.font("HackGen35Nerd"),
+  -- font = wezterm.font("Monaco"),
   window_padding = { left = 0, right = 0, top = 0, bottom = 0, },
-  use_ime = true, -- wezは日本人じゃないのでこれがないとIME動かない
+  use_ime = true,
   font_size = 20.0,
-  color_scheme = "nord", -- 自分の好きなhなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
-  adjust_window_size_when_changing_font_size = false,
-  -- default_cursor_style = "BlinkingBar",
-  -- cursor_blink_rate = 400,
+  -- color_scheme = "AyuMirage (Gogh)",
+  color_scheme = "TokyoNight (Gogh)",
+  djust_window_size_when_changing_font_size = false,
   enable_tab_bar = true,
   use_fancy_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = "TITLE | RESIZE",
-  ative_macos_fullscreen_mode = false,
   window_background_opacity = 1.0,
   colors = {
     foreground = 'silver',
@@ -25,50 +24,14 @@ return {
 
   -- leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
   keys = {
-    {
-      key = 'v',
-      mods = 'SHIFT',
-      action = wezterm.action.ActivateCopyMode,
-    },
-    {
-      key = 'd',
-      mods = 'CMD',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'd',
-      mods = 'CMD|SHIFT',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = '[',
-      mods = 'CMD',
-      action = wezterm.action.ActivatePaneDirection "Prev",
-    },
-    {
-      key = ']',
-      mods = 'CMD',
-      action = wezterm.action.ActivatePaneDirection 'Next',
-    },
-    {
-      key = 'LeftArrow',
-      mods = 'CMD',
-      action = wezterm.action.ActivatePaneDirection "Left",
-    },
-    {
-      key = 'RightArrow',
-      mods = 'CMD',
-      action = wezterm.action.ActivatePaneDirection 'Right',
-    },
-    {
-      key = 'UpArrow',
-      mods = 'CMD',
-      action = wezterm.action.ActivatePaneDirection 'Up',
-    },
-    {
-      key = 'DownArrow',
-      mods = 'CMD',
-      action = wezterm.action.ActivatePaneDirection 'Down',
-    },
+    { key = 'v',          mods = 'CTRL',      action = wezterm.action.ActivateCopyMode,                                 },
+    { key = 'd',          mods = 'CMD',       action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+    { key = 'd',          mods = 'CMD|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },   },
+    { key = '[',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection "Prev",                     },
+    { key = ']',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Next',                     },
+    { key = 'LeftArrow',  mods = 'CMD',       action = wezterm.action.ActivatePaneDirection "Left",                     },
+    { key = 'RightArrow', mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Right',                    },
+    { key = 'UpArrow',    mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Up',                       },
+    { key = 'DownArrow',  mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Down',                     },
   }
 }
