50a352f1198ce56041686d555e8e0ed12e41510d
Takao <k.takao@neogenia.co.jp>
2022-11-04 11:06:37 +0900

    fix: wezterm copymodeがvimのデクリメントとバッティングするので変更した。

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -24,14 +24,14 @@ return {
 
   -- leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
   keys = {
-    { key = 'v',          mods = 'CTRL',      action = wezterm.action.ActivateCopyMode,                                 },
-    { key = 'd',          mods = 'CMD',       action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
-    { key = 'd',          mods = 'CMD|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },   },
-    { key = '[',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection "Prev",                     },
-    { key = ']',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Next',                     },
-    { key = 'LeftArrow',  mods = 'CMD',       action = wezterm.action.ActivatePaneDirection "Left",                     },
-    { key = 'RightArrow', mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Right',                    },
-    { key = 'UpArrow',    mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Up',                       },
-    { key = 'DownArrow',  mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Down',                     },
+    { key = 'x',          mods = 'CTRL|SHIFT', action = wezterm.action.ActivateCopyMode,                                 },
+    { key = 'd',          mods = 'CMD',        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+    { key = 'd',          mods = 'CMD|SHIFT',  action = wezterm.action.SplitVertical   { domain = 'CurrentPaneDomain' }, },
+    { key = '[',          mods = 'CMD',        action = wezterm.action.ActivatePaneDirection "Prev",                     },
+    { key = ']',          mods = 'CMD',        action = wezterm.action.ActivatePaneDirection 'Next',                     },
+    { key = 'LeftArrow',  mods = 'CMD',        action = wezterm.action.ActivatePaneDirection "Left",                     },
+    { key = 'RightArrow', mods = 'CMD',        action = wezterm.action.ActivatePaneDirection 'Right',                    },
+    { key = 'UpArrow',    mods = 'CMD',        action = wezterm.action.ActivatePaneDirection 'Up',                       },
+    { key = 'DownArrow',  mods = 'CMD',        action = wezterm.action.ActivatePaneDirection 'Down',                     },
   }
 }
