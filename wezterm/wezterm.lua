3fc09c0a20813e1ad7b272e4d012bd84d63fe149
Takao <k.takao@neogenia.co.jp>
2022-11-04 11:07:41 +0900

    fix: HackGen35で「設定」などの全角文字が表示できないので cica に変更した。

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,17 @@
 local wezterm = require 'wezterm';
 
 return {
-  font = wezterm.font("HackGen35Nerd"),
-  -- font = wezterm.font("Monaco"),
+  font = wezterm.font_with_fallback {
+    'Cica',
+    'HackGen35 Console',
+    'HackGen35',
+    'JetBrains Mono',
+    'Ricty Diminished Discord',
+    'Monaco',
+  },
   window_padding = { left = 0, right = 0, top = 0, bottom = 0, },
   use_ime = true,
-  font_size = 20.0,
+  font_size = 24,
   -- color_scheme = "AyuMirage (Gogh)",
   color_scheme = "TokyoNight (Gogh)",
   djust_window_size_when_changing_font_size = false,
@@ -14,13 +20,6 @@ return {
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = "TITLE | RESIZE",
   window_background_opacity = 1.0,
-  colors = {
-    foreground = 'silver',
-    -- background = 'black',
-    -- cursor_bg = '#52ad70',
-    -- cursor_fg = 'black',
-    -- cursor_border = '#52ad70',
-  },
 
   -- leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
   keys = {
