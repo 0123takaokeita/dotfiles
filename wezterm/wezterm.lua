10dfdf1249faca706158473af822c2daf1c44674
Takao <k.takao@neogenia.co.jp>
2022-12-07 09:51:21 +0900

    feat: 画面透過の設定を追加

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,17 @@ return {
     'Ricty Diminished Discord',
     'Monaco',
   },
+
+  wezterm.on('toggle-opacity', function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    if not overrides.window_background_opacity then
+      overrides.window_background_opacity = 0.7
+    else
+      overrides.window_background_opacity = nil
+    end
+    window:set_config_overrides(overrides)
+  end),
+
   window_padding = { left = 0, right = 0, top = 0, bottom = 0, },
   use_ime = true,
   font_size = 24,
@@ -22,7 +33,8 @@ return {
 
   -- leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
   keys = {
-    { key = 'x',          mods = 'CTRL|SHIFT', action = wezterm.action.ActivateCopyMode,                                 },
+    { key = 'G',          mods = 'CMD',        action = wezterm.action.EmitEvent 'toggle-opacity',                       },
+    { key = 'X',          mods = 'CMD',        action = wezterm.action.ActivateCopyMode,                                 },
     { key = 'd',          mods = 'CMD',        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
     { key = 'd',          mods = 'CMD|SHIFT',  action = wezterm.action.SplitVertical   { domain = 'CurrentPaneDomain' }, },
     { key = '[',          mods = 'CMD',        action = wezterm.action.ActivatePaneDirection "Prev",                     },
