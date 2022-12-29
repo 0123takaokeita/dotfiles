2cd2c964f0dcbae1fa07a808e88af22338af8909
Takao <k.takao@neogenia.co.jp>
2022-12-29 13:48:42 +0900

    feat: add rotate panes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -17,7 +17,7 @@ return {
   wezterm.on('toggle-opacity', function(window, pane)
     local overrides = window:get_config_overrides() or {}
     if not overrides.window_background_opacity then
-      overrides.window_background_opacity = 0.9
+      overrides.window_background_opacity = 1
     else
       overrides.window_background_opacity = nil
     end
@@ -28,24 +28,27 @@ return {
   use_ime = true,
   font_size = 22,
   color_scheme = "tokyonight",
-  djust_window_size_when_changing_font_size = false,
+  adjust_window_size_when_changing_font_size = false,
   enable_tab_bar = true,
   use_fancy_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = "TITLE | RESIZE",
-  window_background_opacity = 1.0,
+  window_background_opacity = 0.92,
+  text_background_opacity = 0.70,
 
   -- leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
   keys = {
-    { key = 'G',          mods = 'CMD',        action = wezterm.action.EmitEvent 'toggle-opacity',                       },
-    { key = 'X',          mods = 'CMD',        action = wezterm.action.ActivateCopyMode,                                 },
     { key = 'd',          mods = 'CMD',        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
     { key = 'd',          mods = 'CMD|SHIFT',  action = wezterm.action.SplitVertical   { domain = 'CurrentPaneDomain' }, },
-    { key = '[',          mods = 'CMD',        action = wezterm.action.ActivatePaneDirection "Prev",                     },
-    { key = ']',          mods = 'CMD',        action = wezterm.action.ActivatePaneDirection 'Next',                     },
-    { key = 'LeftArrow',  mods = 'CMD',        action = wezterm.action.ActivatePaneDirection "Left",                     },
-    { key = 'RightArrow', mods = 'CMD',        action = wezterm.action.ActivatePaneDirection 'Right',                    },
-    { key = 'UpArrow',    mods = 'CMD',        action = wezterm.action.ActivatePaneDirection 'Up',                       },
-    { key = 'DownArrow',  mods = 'CMD',        action = wezterm.action.ActivatePaneDirection 'Down',                     },
+    { key = 'X',          mods = 'CMD', action = wezterm.action.ActivateCopyMode,     },
+    { key = 'G',          mods = 'CMD', action = wezterm.action.EmitEvent             'toggle-opacity',   },
+    { key = '[',          mods = 'CMD', action = wezterm.action.ActivatePaneDirection "Prev",             },
+    { key = ']',          mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Next',             },
+    { key = 'LeftArrow',  mods = 'CMD', action = wezterm.action.ActivatePaneDirection "Left",             },
+    { key = 'RightArrow', mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Right',            },
+    { key = 'UpArrow',    mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Up',               },
+    { key = 'DownArrow',  mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Down',             },
+    { key = 'j',          mods = 'CMD', action = wezterm.action.RotatePanes           'Clockwise'         },
+    { key = 'k',          mods = 'CMD', action = wezterm.action.RotatePanes           'CounterClockwise', },
   }
 }
