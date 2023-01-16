8ec6535d0c1c309297d92337dfb40e4e2c0238ae
Takao <k.takao@neogenia.co.jp>
2023-01-16 22:37:09 +0900

    fix: exam ruby setup lspserver solagraph

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,7 @@ key.set('n', '<Leader>e', '<cmd>e ~/.config/nvim/init.lua<cr>')
 key.set('n', '<Leader><Leader>', '<cmd>source  ~/.config/nvim/init.lua<cr> <cmd>lua print("Reloaded init.lua")<cr>')
 key.set('n', '*','*N')
 key.set('n', '<Leader>zf', '<cmd>set foldmethod=indent<cr>')
+key.set('n', '<Leader>m', '<cmd>Mason<cr>')
 
 -- Open LazyGit
 key.set('n', 'gl', ':LazyGit<CR>')
@@ -50,9 +51,6 @@ key.set('n', '-', '<C-x>')
 -- Select all
 key.set('n', '<C-a>', 'gg<S-v>G')
 
---No search highlight 
-key.set('n', '<esc><esc>', '<cmd>noh<cr>')
-
 -- Surround
 key.set('n', 'ff', '<plug>Csurround"\'')
 key.set('n', 'tt', '<plug>Csurround\'"')
@@ -69,14 +67,14 @@ opt.expandtab     = true
 opt.tabstop       = 4     -- tag入力の変更
 opt.shiftwidth    = 2
 opt.cursorline    = true
-opt.swapfile      = false
+opt.swapfile      = false -- swapfileを作らない
 opt.wrap          = false -- 折返し無効
 opt.termguicolors = true  -- カラースキームのために
-opt.helplang      = 'ja' -- help 日本語化
-opt.splitright    = true -- split時に右側に表示する
-opt.splitbelow    = true -- split時に下側に表示する
+opt.helplang      = 'ja'  -- help 日本語化
+opt.splitright    = true  -- split時に右側に表示する
+opt.splitbelow    = true  -- split時に下側に表示する
 opt.autoread      = true
-opt.ignorecase    = true -- 検索時に大文字小文字を区別しない
+opt.ignorecase    = true  -- 検索時に大文字小文字を区別しない
 opt.winblend      = 6
 
 
@@ -85,21 +83,56 @@ cmd([[
   colorscheme tokyonight-night
 ]])
 
+-- nvim-cmp
+local nvim_cmp_config = function()
+  local cmp = require('cmp')
+  cmp.setup({
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    preselect = cmp.PreselectMode.None,
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ['<Tab>'] = cmp.mapping.complete(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = {
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' },
+      { name = 'buffer', option = {
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(api.nvim_list_wins()) do
+            bufs[api.nvim_win_get_buf(win)] = true
+          end
+          return vim.tbl_keys(bufs)
+        end
+      } },
+      { name = 'path' },
+    },
+    snippet = {
+      expand = function(args)
+        fn['vsnip#anonymous'](args.body)
+      end
+    },
+  })
+end
+
 return require('packer').startup(function()
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'                 -- plugin manager
   use 'tpope/vim-surround'                     -- ex: text object operation. key: <>S([
   use 'tpope/vim-commentary'                   -- comment out key:gcc
-  use 'lukas-reineke/indent-blankline.nvim'    -- indent guide
-  use {'lambdalisue/fern.vim'}                 -- filer
-  use {'lambdalisue/nerdfont.vim',}            -- filer icon
+  use 'lambdalisue/fern.vim'                  -- filer
+  use 'lambdalisue/nerdfont.vim'            -- filer icon
     g['fern#renderer'] = 'nerdfont'
     g['fern#default_hidden'] = true
 
   use 'lambdalisue/fern-renderer-nerdfont.vim'
   use 'lambdalisue/fern-git-status.vim'
-  use 'lambdalisue/glyph-palette.vim'
-  use 'lambdalisue/fern-bookmark.vim'
 
   use 'machakann/vim-highlightedyank'          -- yank highlight
     g.highlightedyank_highlight_duration = 100 -- duration time
@@ -116,7 +149,6 @@ return require('packer').startup(function()
     g.lazygit_floating_window_winblend       = 0 -- window transparency
 
 
-
   use {
    'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
@@ -124,17 +156,17 @@ return require('packer').startup(function()
 
   use 'folke/tokyonight.nvim'
   use {
-	"folke/todo-comments.nvim",
-	requires = "nvim-lua/plenary.nvim",
-	config = function()
-	  require("todo-comments").setup {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	  }
-	end
+   "folke/todo-comments.nvim",
+   requires = "nvim-lua/plenary.nvim",
+   config = function()
+     require("todo-comments").setup {
+       -- your configuration comes here
+       -- or leave it empty to use the default settings
+       -- refer to the configuration section below
+     }
+   end
   }
-
+ 
   use({"folke/noice.nvim",
 	config = function()
 	  require("noice").setup({
@@ -157,4 +189,84 @@ return require('packer').startup(function()
 	 "rcarriga/nvim-notify",
      }
   })
+
+  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+  use { "williamboman/mason.nvim" }
+  require("mason").setup({
+   ui = {
+       icons = {
+           package_installed   = "✓",
+           package_pending     = "➜",
+           package_uninstalled = "✗"
+       }
+     }
+  })
+
+  use 'williamboman/mason-lspconfig.nvim'
+  require("mason-lspconfig").setup({
+    ensure_installed = { 'sumneko_lua', 'rust_analyzer', 'ruby_ls' },
+    automatic_installation = true,
+  })
+
+  -- TODO: Progress
+  --
+  -- complete
+  use {
+    'hrsh7th/nvim-cmp',
+    module = { "cmp" },
+    requires = {
+      { 'hrsh7th/cmp-nvim-lsp', event = { 'InsertEnter' } },
+      { 'hrsh7th/cmp-buffer', event = { 'InsertEnter' } },
+      { 'hrsh7th/cmp-path', event = { 'InsertEnter' } },
+      { 'hrsh7th/cmp-vsnip', event = { 'InsertEnter' } },
+      { 'hrsh7th/vim-vsnip', event = { 'InsertEnter' } },
+      { 'hrsh7th/cmp-cmdline', event = { 'CmdlineEnter' } },
+    },
+    config = nvim_cmp_config,
+  }
+
+  require'lspconfig'.solargraph.setup{}
+  local nvim_lsp = require('lspconfig')
+  local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    --Enable completion triggered by <c-x><c-o>
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    local opts = { noremap=true, silent=true }
+
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    buf_set_keymap('n', 'gD',        '<Cmd>lua vim.lsp.buf.declaration()<CR>',                                opts)
+    buf_set_keymap('n', 'gd',        '<Cmd>lua vim.lsp.buf.definition()<CR>',                                 opts)
+    buf_set_keymap('n', 'K',         '<Cmd>lua vim.lsp.buf.hover()<CR>',                                      opts)
+    buf_set_keymap('n', 'gi',        '<cmd>lua vim.lsp.buf.implementation()<CR>',                             opts)
+    buf_set_keymap('n', '<C-k>',     '<cmd>lua vim.lsp.buf.signature_help()<CR>',                             opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',                       opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',                    opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D',  '<cmd>lua vim.lsp.buf.type_definition()<CR>',                            opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>',                                     opts)
+    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',                                opts)
+    buf_set_keymap('n', 'gr',        '<cmd>lua vim.lsp.buf.references()<CR>',                                 opts)
+    buf_set_keymap('n', '<space>e',  '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',               opts)
+    buf_set_keymap('n', '[d',        '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',                           opts)
+    buf_set_keymap('n', ']d',        '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',                           opts)
+    buf_set_keymap('n', '<space>q',  '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',                         opts)
+    buf_set_keymap("n", "<space>f",  "<cmd>lua vim.lsp.buf.formatting()<CR>",                                 opts)
+  end
+  -- Use a loop to conveniently call 'setup' on multiple servers and
+  -- map buffer local keybindings when the language server attaches
+  local servers = { "solargraph" }
+  for _, lsp in ipairs(servers) do
+    nvim_lsp[lsp].setup {
+      on_attach = on_attach,
+      flags = {
+        debounce_text_changes = 150,
+      }
+    }
+  end
+
+  -- END
 end)
