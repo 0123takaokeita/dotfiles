2ba438c672cefb1a2b18cb4fca4879b771c4a7a0
Takao <k.takao@neogenia.co.jp>
2023-01-17 10:40:11 +0900

    feat: add telescope icons

--- nvim/init.lua
+++ nvim/init.lua
@@ -151,7 +151,10 @@ return require('packer').startup(function()
 
   use {
    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = {
+        'nvim-lua/plenary.nvim',
+        'kyazdani42/nvim-web-devicons',
+    }
   }
 
   use 'folke/tokyonight.nvim'
