80c36721b2a6d252006f37a0c360974848a8743a
Takao <k.takao@neogenia.co.jp>
2023-01-20 11:07:02 +0900

    feat: lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -410,4 +410,10 @@ return require('packer').startup(function()
     },
   }
 
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    require('lualine').setup {
+    }
+  }
 end)
