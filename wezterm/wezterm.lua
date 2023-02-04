cb5f3c70da9b15a21b3bb583d08d01142a29081b
Takao <k.takao@neogenia.co.jp>
2023-02-04 22:49:56 +0900

    feat: add tab color orange

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,6 +14,26 @@ return {
     'Monaco',
   },
 
+  color_scheme = 'nightfox',
+  colors = {
+    tab_bar = {
+      background = '#0b0022',
+      inactive_tab_edge = '#575757',
+      active_tab = {
+         bg_color = '#ffcc66',
+         fg_color = '#0f0f3d',
+         intensity = 'Bold', -- Half or Bold or Normal
+         underline = 'Double', -- None or Single or Double
+         italic = true,
+         strikethrough = true,
+      },
+      new_tab = {
+        bg_color = '#1b1032',
+        fg_color = '#808080',
+      },
+    },
+  },
+
   wezterm.on('toggle-opacity', function(window, pane)
     local overrides = window:get_config_overrides() or {}
     if not overrides.window_background_opacity then
@@ -24,19 +44,19 @@ return {
     window:set_config_overrides(overrides)
   end),
 
+  tab_max_width = 16,
   window_padding = { left = 0, right = 0, top = 0, bottom = 0, },
   use_ime = true,
   initial_rows = 40,
   initial_cols = 90,
-  font_size = 19,
-  color_scheme = "nightfox",
-  adjust_window_size_when_changing_font_size = false,
+  font_size = 21,
+  adjust_window_size_when_changing_font_size = true,
   enable_tab_bar = true,
   use_fancy_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = "TITLE | RESIZE",
-  window_background_opacity = 0.80,
-  text_background_opacity = 0.70,
+  window_background_opacity = 0.95,
+  text_background_opacity = 0.90,
 
   -- leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
   keys = {
