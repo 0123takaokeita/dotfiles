6618e641585bc749db6ff0c66cdfa8e2d1453c14
Takao <k.takao@neogenia.co.jp>
2023-04-15 20:41:34 +0900

    fix: weztermのフォントなど調整

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,33 +1,30 @@
 local wezterm = require 'wezterm';
 
 return {
-  font = wezterm.font_with_fallback( {
-    'JetBrains Mono',
-    'Cica',
-    'UDEV Gothic 35NF',
-    'HackGenNerd',
-    'Monaco',
-    'UDEV Gothic NF',
-    'HackGenNerd Console',
-    'HackGen',
-    'HackGen Console',
-    'HackGen Console NF ',
-  },
-    {weight = 'Bold'}
+  -- フォントの設定
+  font = wezterm.font_with_fallback({
+      'JetBrains Mono',
+      'UDEV Gothic 35NF',
+      'Cica',
+      'Monaco',
+      'UDEV Gothic NF',
+    },
+    -- 文字の太さ
+    { weight = 'Bold' }
   ),
-
+  --  テーマ設定
   color_scheme = 'nightfox',
   colors = {
     tab_bar = {
       background = '#0b0022',
       inactive_tab_edge = '#575757',
       active_tab = {
-         bg_color = '#ffcc66',
-         fg_color = '#0f0f3d',
-         intensity = 'Bold', -- Half or Bold or Normal
-         underline = 'Double', -- None or Single or Double
-         italic = true,
-         strikethrough = true,
+        bg_color = '#ffcc66',
+        fg_color = '#0f0f3d',
+        intensity = 'Bold',    -- Half or Bold or Normal
+        underline = 'Double',  -- None or Single or Double
+        italic = true,
+        strikethrough = true,
       },
       new_tab = {
         bg_color = '#1b1032',
@@ -35,7 +32,7 @@ return {
       },
     },
   },
-
+  -- 透明度の設定
   wezterm.on('toggle-opacity', function(window, pane)
     local overrides = window:get_config_overrides() or {}
     if not overrides.window_background_opacity then
@@ -52,28 +49,28 @@ return {
   initial_cols = 120,
   initial_rows = 80,
   window_close_confirmation = "NeverPrompt",
-  font_size = 21,
+  font_size = 22,
   adjust_window_size_when_changing_font_size = true,
   enable_tab_bar = true,
   use_fancy_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = "TITLE | RESIZE",
   window_background_opacity = 0.95,
-  text_background_opacity = 0.90,
-
+  text_background_opacity = 0.95,
+  -- keymapのoverride
   -- leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
   keys = {
-    { key = 'd',          mods = 'CMD',        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
-    { key = 'd',          mods = 'CMD|SHIFT',  action = wezterm.action.SplitVertical   { domain = 'CurrentPaneDomain' }, },
-    { key = 'X',          mods = 'CMD', action = wezterm.action.ActivateCopyMode,     },
-    { key = 'G',          mods = 'CMD', action = wezterm.action.EmitEvent             'toggle-opacity',   },
-    { key = '[',          mods = 'CMD', action = wezterm.action.ActivatePaneDirection "Prev",             },
-    { key = ']',          mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Next',             },
-    { key = 'LeftArrow',  mods = 'CMD', action = wezterm.action.ActivatePaneDirection "Left",             },
-    { key = 'RightArrow', mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Right',            },
-    { key = 'UpArrow',    mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Up',               },
-    { key = 'DownArrow',  mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Down',             },
-    { key = 'j',          mods = 'CMD', action = wezterm.action.RotatePanes           'Clockwise'         },
-    { key = 'k',          mods = 'CMD', action = wezterm.action.RotatePanes           'CounterClockwise', },
+    { key = 'd',          mods = 'CMD',       action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+    { key = 'd',          mods = 'CMD|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }, },
+    { key = 'X',          mods = 'CMD',       action = wezterm.action.ActivateCopyMode, },
+    { key = 'G',          mods = 'CMD',       action = wezterm.action.EmitEvent 'toggle-opacity', },
+    { key = '[',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection "Prev", },
+    { key = ']',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Next', },
+    { key = 'LeftArrow',  mods = 'CMD',       action = wezterm.action.ActivatePaneDirection "Left", },
+    { key = 'RightArrow', mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Right', },
+    { key = 'UpArrow',    mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Up', },
+    { key = 'DownArrow',  mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Down', },
+    { key = 'j',          mods = 'CMD',       action = wezterm.action.RotatePanes 'Clockwise' },
+    { key = 'k',          mods = 'CMD',       action = wezterm.action.RotatePanes 'CounterClockwise', },
   }
 }
