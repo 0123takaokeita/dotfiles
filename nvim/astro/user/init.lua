75d4d3c371df4cbe5f51d9ec0e72f4b4cf9db7d6
Takao <k.takao@neogenia.co.jp>
2023-04-24 11:20:47 +0900

    feat: add wakatime to astro config

--- nvim/astro/user/init.lua
+++ nvim/astro/user/init.lua
@@ -44,6 +44,10 @@ return {
       end,
     },
     {
+      'wakatime/vim-wakatime',
+      event = 'VeryLazy',
+    },
+    {
       'kylechui/nvim-surround',
       version = '*', -- Use for stability; omit to use `main` branch for the latest features
       event = 'VeryLazy',
