361a4dd9c7008bd0ea6926d93b25d184370fa6dc
KeitaTakao <k.takao@neogenia.co.jp>
2023-08-11 23:30:54 +0900

    fix: update fish config

--- fish/neo-config.fish
+++ fish/neo-config.fish
@@ -4,19 +4,24 @@ set -x XDG_CONFIG_HOME $HOME/.config
 set -x MYVIMRC $HOME/.config/nvim/init.vim
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
-set -x LESS -i -M -R -S -W -z-4 -x4 # less コマンドデフォルトオプション
 set -x GITHUB_UNAME 0123takaokeita
 set -x PYENV_ROOT $HOME/.pyenv
 set -x ANDROID_HOME $HOME/Library/Android/sdk
 set -x CalcSvcClass CalcSvcTakao
 
+set -x LESS -i -M -R -W -z-4 -x4 # less コマンドデフォルトオプション -S は折り返しを無効にする
+# set -x LESSOPEN "| src-hilite-lesspipe.sh %s"
+
+set GHQ_SELECTOR peco # C-g でghq list peco
+set fish_plugins theme peco
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)' # C-r command history peco
+end
+
+
 # Android config
 fish_add_path $ANDROID_HOME/emulator
 fish_add_path $ANDROID_HOME/platform-tools
-
-# rbrenv config
-fish_add_path $HOME/.rbenv/shims
-status --is-interactive; and source (rbenv init -|psub)
 
 fish_add_path $HOME/go/bin # go 設定
 fish_add_path $HOME/.volta/bin # volta 設定
@@ -51,10 +56,11 @@ alias sfish='source ~/.config/fish/config.fish'
 alias osaka='curl wttr.in/Osaka'
 
 # cd directory
-alias dot='cd ~/dotfiles; neo'
-alias bys='cd ~/vm_share/byYourSide; neo'
-alias rel='cd ~/vm_share/relief-map/; neo'
-alias calc='cd ~/dev/github.com/lobin-z0x50/NeoCalc; neo'
+alias dot='cd ~/dotfiles; vim fish/neo-config.fish'
+alias bys='cd ~/vm_share/byYourSide; vim'
+alias rel='cd ~/vm_share/relief-map/; vim'
+alias calc='cd ~/dev/github.com/lobin-z0x50/NeoCalc/CalcLibCore/Takao; neo'
+
 alias .='cd'
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -68,6 +74,10 @@ alias dev='cd $(ghq root)/$(ghq list | peco)'
 
 # pecoで選択したHostに接続
 alias sshl='grep -w Host ~/.ssh/config | peco | awk \'{print $2}\' | xargs -o -n 1 ssh'
+
+# rbrenv config
+fish_add_path $HOME/.rbenv/shims
+status --is-interactive; and source (rbenv init -|psub)
 
 starship init fish | source
 
