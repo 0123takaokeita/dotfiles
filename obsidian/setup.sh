d84ad4f802a13dd2044858cfdbb2360c6c524ae1
KeitaTakao <k.takao@neogenia.co.jp>
2023-08-23 01:10:11 +0900

    feat: add obsidian snippets config

---
+++ obsidian/setup.sh
@@ -0,0 +1,4 @@
+# css snippets file
+ln -fs $HOME/dotfiles/obsidian/snippets $HOME/obsidian_valto/.obsidian/snippets
+# vim extention config file
+ln -fs $HOME/dotfiles/obsidian/obsidian.vimrc $HOME/obsidian_valto/.obsidian.vimrc
