192e4491dc63bda37df954c57c5311f3351c29ac
KeitaTakao <k.takao@neogenia.co.jp>
2023-08-23 01:09:25 +0900

    fix: wezterm edit appearance

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,15 +1,16 @@
 local wezterm = require 'wezterm';
+local ac = wezterm.action
 
 return {
   -- フォントの設定
   font = wezterm.font_with_fallback({
+      'JetBrains Mono',
+      'Fira Mono for Powerline',
       'Cica',
       'UDEV Gothic 35NF',
       'Monaco',
-      'JetBrains Mono',
       'UDEV Gothic NF',
-    }
-    ,
+    },
     -- 文字の太さ
     { weight = 'Bold' }
   ),
@@ -23,8 +24,8 @@ return {
       active_tab = {
         bg_color = '#FFA500',
         fg_color = '#0f0f3d',
-        intensity = 'Bold',    -- Half or Bold or Normal
-        underline = 'Double',  -- None or Single or Double
+        intensity = 'Bold',   -- Half or Bold or Normal
+        underline = 'Double', -- None or Single or Double
         italic = true,
         strikethrough = true,
       },
@@ -45,34 +46,34 @@ return {
     window:set_config_overrides(overrides)
   end),
 
-  tab_max_width = 16,
-  window_padding = { left = 0, right = 0, top = 0, bottom = 0, },
+  tab_max_width = 20,
+  window_padding = { left = 5, right = 5, top = 5, bottom = 5, },
   use_ime = true,
   initial_cols = 120,
   initial_rows = 80,
   window_close_confirmation = "NeverPrompt",
-  font_size = 22,
+  font_size = 20,
   adjust_window_size_when_changing_font_size = true,
   enable_tab_bar = true,
   use_fancy_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = "TITLE | RESIZE",
-  window_background_opacity = 0.95,
-  text_background_opacity = 0.95,
+  window_background_opacity = 0.90,
+  text_background_opacity = 0.90,
   -- keymapのoverride
   -- leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
   keys = {
-    { key = 'd',          mods = 'CMD',       action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
-    { key = 'd',          mods = 'CMD|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }, },
-    { key = 'X',          mods = 'CMD',       action = wezterm.action.ActivateCopyMode, },
-    { key = 'G',          mods = 'CMD',       action = wezterm.action.EmitEvent 'toggle-opacity', },
-    { key = '[',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection "Prev", },
-    { key = ']',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Next', },
-    { key = 'LeftArrow',  mods = 'CMD',       action = wezterm.action.ActivatePaneDirection "Left", },
-    { key = 'RightArrow', mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Right', },
-    { key = 'UpArrow',    mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Up', },
-    { key = 'DownArrow',  mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Down', },
-    { key = 'j',          mods = 'CMD',       action = wezterm.action.RotatePanes 'Clockwise' },
-    { key = 'k',          mods = 'CMD',       action = wezterm.action.RotatePanes 'CounterClockwise', },
+    { key = 'd',          mods = 'CMD',       action = ac.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+    { key = 'd',          mods = 'CMD|SHIFT', action = ac.SplitVertical { domain = 'CurrentPaneDomain' }, },
+    { key = 'X',          mods = 'CMD',       action = ac.ActivateCopyMode, },
+    { key = 'G',          mods = 'CMD',       action = ac.EmitEvent 'toggle-opacity', },
+    { key = '[',          mods = 'CMD',       action = ac.ActivatePaneDirection "Prev", },
+    { key = ']',          mods = 'CMD',       action = ac.ActivatePaneDirection 'Next', },
+    { key = 'LeftArrow',  mods = 'CMD',       action = ac.ActivatePaneDirection "Left", },
+    { key = 'RightArrow', mods = 'CMD',       action = ac.ActivatePaneDirection 'Right', },
+    { key = 'UpArrow',    mods = 'CMD',       action = ac.ActivatePaneDirection 'Up', },
+    { key = 'DownArrow',  mods = 'CMD',       action = ac.ActivatePaneDirection 'Down', },
+    { key = 'j',          mods = 'CMD',       action = ac.RotatePanes 'Clockwise' },
+    { key = 'k',          mods = 'CMD',       action = ac.RotatePanes 'CounterClockwise', },
   }
 }
