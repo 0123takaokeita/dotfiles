a805ebcfea37018a86d04933c2a3f1f698b75f19
keita_takao <keita_takao@tkm-macbook-pro.local>
2024-03-16 00:58:06 +0900

    add comment  brew setup.sh

--- brew/setup.sh
+++ brew/setup.sh
@@ -1,3 +1,7 @@
 # HOMEにBrewfileを作成する
+echo "--- link dotfiles .Brewfile ---"
 ln -fs $HOME/dotfiles/brew/.Brewfile $HOME/.Brewfile
+
+echo "--- START brew bundle ---"
 brew bundle --global
+echo "--- END brew bundle ---"
